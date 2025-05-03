69e33bc6337c8303a58b9a56aa2ed914e97402e0
dogukanVaris <varisdogukan@outlook.com>
2025-05-03 15:07:35 +0300

    Add Product Card

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,5 @@
+import ProductCard from "./product-card";
+
 const ProductList = ({
   data,
   title,
@@ -16,7 +18,7 @@ const ProductList = ({
       {data.length > 0 ? (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3.5'>
           {limitedData.map((product: any) => (
-            <div key={product.name}>{product.name}</div>
+            <ProductCard key={product.slug} product={product} />
           ))}
         </div>
       ) : (
