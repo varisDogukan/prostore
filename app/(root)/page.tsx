89e7d4d3608337a50eb1bb549e5f892af2902421
dogukanVaris <varisdogukan@outlook.com>
2025-05-03 14:59:11 +0300

    Add Sample Product & Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = () => {
-  return <>ProStore</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default HomePage;
