bb0cbbcaaa251312b4cea0fe376a12af7cacdcab
dogukanVaris <varisdogukan@outlook.com>
2025-05-04 10:31:51 +0300

    Load Products From Database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,12 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title='Newest Arrivals'
-        limit={4}
-      />
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
     </>
   );
 };
