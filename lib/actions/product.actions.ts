ac98359d9586c78b0753785802c68357c83ace38
dogukanVaris <varisdogukan@outlook.com>
2025-05-10 08:38:11 +0300

    Add Serverless  Environment Config

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,13 +1,11 @@
 "use server";
 
-import { PrismaClient } from "../generated/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma";
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
