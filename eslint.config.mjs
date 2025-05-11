eb97056624381044c324d745bafc40767797a0ad
dogukanVaris <varisdogukan@outlook.com>
2025-05-11 07:20:07 +0300

    test fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "dist/**",
+      "out/**",
+      "build/**",
+      "lib/generated/**",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
