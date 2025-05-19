6f256fa12af738ed808bf60a57479288373e345f
@varis.dogukan <dogukan.varis@glomil.com>
2025-05-19 13:05:57 +0300

    Add Payment Method Form & Update

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { auth } from "@/auth";
 import { getUserById } from "@/lib/actions/user.actions";
 import PaymentMethodForm from "./payment-method-form";
+import CheckoutSteps from "@/components/shared/checkout-steps";
 
 export const metadata: Metadata = {
   title: "Select Payment Method",
@@ -17,6 +18,7 @@ const PaymentMethodPage = async () => {
 
   return (
     <>
+      <CheckoutSteps current={2} />
       <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
     </>
   );
